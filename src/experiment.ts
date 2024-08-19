423afb2aac44f0663ad3d283fc0cdb61d09883bf
Ember Zhang <emberlzhang@gmail.com>
2024-08-19 12:16:09 -0400

    update stage2 alien gem probabilities

--- src/experiment.ts
+++ src/experiment.ts
@@ -1,6 +1,6 @@
 let round: number = 1;
 let practiceRounds: number = 10;
-let mainRounds: number = 150; 
+let mainRounds: number = 150;
 let totalRounds: number = practiceRounds;
 let points: number = 0;
 let currentStage: string = "intake"; // [intake, welcome, instructions1, instructions2, instructions3, practiceStage1, practiceStage2, instructionsFinal, mainStage1, mainStage2]
@@ -14,18 +14,18 @@ let intertrialInterval2: number = 0;
 // console.log("intertrialInterval2: " + intertrialInterval2)
 
 // variables for game setting 1
-const REWARD_1 = { points: 100, image: "reward-img-gem", message: "You found a gem (+100 points)!"};
-const REWARD_2 = { points: 0, image: "reward-img-dirt", message: "You found dirt (no points)!"};
+const REWARD_1 = { points: 100, image: "reward-img-gem", message: "You found a gem (+100 points)!" };
+const REWARD_2 = { points: 0, image: "reward-img-dirt", message: "You found dirt (no points)!" };
 // // variables for game setting 2
 // const REWARD_1 = { points: 100, image: "img/gem-sapphire.png" };
 // const REWARD_2 = { points: 100, image: "img/gem-ruby.png" };
 
 // likelihoods for game setting 1
 const stage2Options = {
-    "A": { likelihoods: [0.7,0.6,0.7,0.6,0.6,0.6,0.5,0.5,0.5,0.5,0.5,0.6,0.6,0.6,0.7,0.7,0.7,0.7,0.7,0.6,0.6,0.6,0.5,0.5,0.4,0.5,0.5,0.4,0.4,0.5,0.4,0.4,0.5,0.4,0.4,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.4,0.4,0.3,0.3,0.4,0.4,0.3,0.3,0.3,0.3,0.3,0.3,0.3,0.3,0.3,0.3,0.3,0.3,0.3,0.4,0.3,0.3,0.3,0.3,0.3,0.3,0.3,0.3,0.4,0.4,0.4,0.4,0.5,0.5,0.5,0.5,0.5,0.6,0.6,0.6,0.5,0.5,0.6,0.5,0.5,0.5,0.5,0.5,0.4,0.5,0.5,0.6,0.6,0.6,0.5,0.5,0.5,0.5,0.6,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.6,0.6,0.6,0.6,0.6,0.6,0.6,0.6,0.6,0.6,0.6,0.6,0.6,0.6,0.7,0.6,0.5,0.5,0.5,0.5,0.5,0.7,0.6,0.6,0.6,0.5,0.5,0.5,0.5,0.5,0.4,0.4,0.4,0.4,0.4,0.4,0.4,0.4,0.4,0.4,0.4,0.4] },
-    "B": { likelihoods: [0.7,0.7,0.7,0.7,0.7,0.7,0.7,0.7,0.7,0.6,0.6,0.6,0.6,0.6,0.7,0.7,0.6,0.6,0.7,0.7,0.7,0.7,0.7,0.7,0.7,0.7,0.7,0.7,0.7,0.7,0.7,0.6,0.6,0.7,0.6,0.6,0.6,0.5,0.5,0.4,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.4,0.5,0.5,0.5,0.4,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.6,0.6,0.6,0.6,0.7,0.7,0.7,0.7,0.7,0.7,0.7,0.7,0.7,0.7,0.7,0.7,0.7,0.7,0.7,0.7,0.6,0.6,0.6,0.5,0.5,0.5,0.5,0.5,0.5,0.6,0.6,0.6,0.6,0.6,0.6,0.6,0.5,0.6,0.6,0.6,0.5,0.5,0.5,0.5,0.4,0.4,0.4,0.4,0.3,0.3,0.3,0.3,0.4,0.4,0.4,0.5,0.4,0.4,0.3,0.3,0.3,0.3,0.4,0.4,0.4,0.4,0.4,0.4,0.4,0.5,0.4,0.4,0.5,0.5,0.5,0.4,0.4,0.4,0.4,0.4,0.4,0.4,0.4] },
-    "C": { likelihoods: [0.7,0.7,0.7,0.7,0.7,0.7,0.7,0.6,0.6,0.6,0.6,0.5,0.6,0.6,0.6,0.6,0.6,0.6,0.6,0.6,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.4,0.4,0.4,0.4,0.4,0.5,0.5,0.6,0.6,0.5,0.6,0.6,0.7,0.7,0.7,0.7,0.7,0.6,0.6,0.6,0.7,0.6,0.6,0.7,0.7,0.6,0.6,0.6,0.6,0.6,0.7,0.7,0.7,0.7,0.6,0.7,0.6,0.6,0.6,0.6,0.6,0.6,0.5,0.6,0.5,0.5,0.4,0.5,0.5,0.4,0.4,0.4,0.4,0.5,0.4,0.4,0.4,0.4,0.4,0.4,0.4,0.5,0.5,0.5,0.4,0.4,0.4,0.4,0.4,0.4,0.4,0.4,0.4,0.4,0.4,0.4,0.5,0.5,0.5,0.5,0.6,0.6,0.6,0.6,0.6,0.6,0.6,0.7,0.6,0.7,0.7,0.6,0.6,0.6,0.7,0.7,0.7,0.6,0.6,0.6,0.6,0.5,0.5,0.5,0.4,0.4,0.3,0.3,0.4,0.4,0.4,0.3,0.3,0.3,0.3,0.3,0.3,0.3,0.3,0.3,0.3,0.3] },
-    "D": { likelihoods: [0.3,0.3,0.3,0.3,0.3,0.3,0.3,0.3,0.3,0.3,0.3,0.3,0.3,0.4,0.3,0.3,0.3,0.3,0.4,0.4,0.4,0.4,0.4,0.4,0.4,0.4,0.4,0.4,0.4,0.4,0.4,0.4,0.4,0.3,0.3,0.3,0.3,0.3,0.3,0.3,0.3,0.4,0.4,0.4,0.4,0.3,0.4,0.4,0.3,0.4,0.4,0.3,0.4,0.3,0.3,0.3,0.4,0.4,0.4,0.4,0.4,0.4,0.4,0.4,0.4,0.3,0.3,0.3,0.4,0.4,0.3,0.4,0.4,0.4,0.4,0.4,0.5,0.4,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.4,0.4,0.4,0.4,0.4,0.3,0.3,0.5,0.5,0.6,0.6,0.6,0.7,0.7,0.7,0.7,0.7,0.7,0.7,0.7,0.7,0.7,0.7,0.7,0.7,0.7,0.6,0.6,0.6,0.7,0.6,0.6,0.6,0.6,0.7,0.7,0.7,0.7,0.7,0.7,0.6,0.6,0.6,0.5,0.6,0.6,0.6,0.6,0.6,0.5,0.6,0.6,0.6,0.6,0.5,0.5,0.5,0.5,0.5,0.6,0.6] },
+    "A": { likelihoods: [0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5] },
+    "B": { likelihoods: [0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4] },
+    "C": { likelihoods: [0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2] },
+    "D": { likelihoods: [0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8] },
 }
 
 
@@ -36,10 +36,10 @@ function chooseOption(option: string): void {
     let reward: number = 0;
     let rewardImage: string = '';
     let rewardMessage: string = '';
-    
+
     // NOTE: Do not put round counter here because not updated until much later stage
 
-    if (currentStage === "mainStage1" || currentStage === "practiceStage1" ) { // Stage 1: Option X or Y
+    if (currentStage === "mainStage1" || currentStage === "practiceStage1") { // Stage 1: Option X or Y
         intertrialInterval1 = [400, 600, 800][Math.floor(Math.random() * 3)]; // milliseconds
         intertrialInterval2 = 0;
 
@@ -55,16 +55,16 @@ function chooseOption(option: string): void {
         // Introduce intertrialInterval delay between Stage 1 user choice and Stage 2 display
         setTimeout(() => {
             // Determine which key options to display (practice vs. main trials)
-            if (currentStage === "mainStage1") { 
+            if (currentStage === "mainStage1") {
                 // Switch main stages
-                currentStage = "mainStage2"; 
+                currentStage = "mainStage2";
                 // Show Stage 2 Options, hide stage 1 display
                 document.getElementById('stage-1-main-instructions')!.style.display = "none";
                 document.getElementById('stage-2-main-instructions')!.style.display = "block";
                 // document.getElementById('stage-2-key-instruction')!.style.display = 'block'; // needs to be here otherwise first trial of stage 2 doesn't have key instruction
             } else { // currentStage === practiceStage1
                 // Switch practice stages
-                currentStage = "practiceStage2"; 
+                currentStage = "practiceStage2";
                 // Show Stage 2 Practice Options, hide stage 1 practice display
                 document.getElementById('stage-1-practice-instructions')!.style.display = "none";
                 document.getElementById('stage-2-practice-instructions')!.style.display = "block";
@@ -73,28 +73,28 @@ function chooseOption(option: string): void {
             // Show Stage 2 Options, hide stage 1 display (same for both main and practice)
             document.getElementById('stage-2-options')!.style.display = "block";
             document.getElementById('stage-1-options')!.style.display = "none";
-            
+
             // Determine which Planet in Stage 2 to display
-            if (outcome === 'X') { 
+            if (outcome === 'X') {
                 document.getElementById('planet-X-options')!.style.display = "block";
                 document.getElementById('planet-Y-options')!.style.display = "none";
             } else { // Option Y
                 document.getElementById('planet-Y-options')!.style.display = "block";
                 document.getElementById('planet-X-options')!.style.display = "none";
             };
-            
+
             keyInputAllowed = true; // Re-allow keyboard input after intertrial interval ends
 
-        }, intertrialInterval1); 
-        
+        }, intertrialInterval1);
+
     } else if (currentStage === "mainStage2" || currentStage === "practiceStage2") { // Stage 2: Option A, B, C, or D
         intertrialInterval2 = [400, 600, 800][Math.floor(Math.random() * 3)]; // 500, 1000, or 1500 millisecond
 
         const userChoice = stage2Options[option as keyof typeof stage2Options];
-        
+
         if (userChoice) {
             const likelihoods = userChoice.likelihoods;
-            const currentLikelihood = likelihoods[round-1]; // pick the likelihood associated with this round (minus one for array index)
+            const currentLikelihood = likelihoods[round - 1]; // pick the likelihood associated with this round (minus one for array index)
             // console.log("currentLikelihood: " + currentLikelihood)
 
             // select reward from user choice
@@ -109,19 +109,19 @@ function chooseOption(option: string): void {
                 rewardImage = REWARD_2.image;
                 rewardMessage = REWARD_2.message;
             }
-            
+
             // This stuff happens IMMEDIATELY AFTER participant makes a key choice
             points += reward; // needs to happen BEFORE data is saved for the round
-            
+
             // Save user choices into data object
             results.push({ stage: currentStage, round: round, choice: option, outcome: outcome, reward: reward, points: points, rewardImage: rewardImage });
 
             // NOTE: Do not put round counter here because not updated until much later stage
-            
+
             setTimeout(() => { // This stuff happens AFTER the intertrial interval
-                
+
                 // Show reward message
-                    // Replace instructions text with reward message + image, keep stage 2 planet + aliens
+                // Replace instructions text with reward message + image, keep stage 2 planet + aliens
                 document.getElementById('stage-2-main-instructions')!.style.display = 'none';
                 document.getElementById('stage-2-practice-instructions')!.style.display = 'none';
                 document.getElementById('reward-message')!.innerText = rewardMessage;
@@ -129,9 +129,9 @@ function chooseOption(option: string): void {
                 document.getElementById(rewardImage)!.style.display = 'block';
                 document.getElementById('pointCounter')!.innerText = points.toString(); // this updates the point counter as soon as reward is selected, correctly for the practice trials at least
                 document.getElementById('stage-2-key-instruction')!.style.display = 'none'; // dont need???
-                
+
                 setTimeout(() => { // This stuff happens AFTER the 2 second reward display interval
-                    
+
                     // Hide reward message, hide stage 2 planet + aliens
                     document.getElementById('reward-message')!.style.display = 'none';
                     document.getElementById(rewardImage)!.style.display = 'none';
@@ -168,16 +168,16 @@ function chooseOption(option: string): void {
                             endTask();
                         }
                     }
-                    
+
                     // Re-allow keyboard input after reward display ends
                     keyInputAllowed = true;
 
                     // console.log("intertrialInterval2 before timeout: " + intertrialInterval2)
-                    
+
                 }, 2000);
 
             }, intertrialInterval2);
-            
+
         } else {
             console.log("choiceConfig doesn't exist")
         }
@@ -189,7 +189,7 @@ function chooseOption(option: string): void {
 // Make user display in full screen mode upon opening the application
 function requestFullScreen(element: HTMLElement = document.documentElement) {
     // document.documentElement is the entire <html> element of the html,
-        // meaning the entire html page (similar to <body> tag)
+    // meaning the entire html page (similar to <body> tag)
     if (element.requestFullscreen) {
         element.requestFullscreen();
     } else if ((element as any).mozRequestFullScreen) { // Older Firefox
@@ -205,10 +205,10 @@ function requestFullScreen(element: HTMLElement = document.documentElement) {
 
 
 // Event listener for instructions screen
-const handleKeydown = function(event: KeyboardEvent) {
+const handleKeydown = function (event: KeyboardEvent) {
     if (event.key === ' ' || event.key === 'Spacebar') {
         console.log("first key event logged")
-        
+
         if (currentStage == "intake") {
             requestFullScreen(); // Make the entire screen full-screen
             document.getElementById('intake-screen')!.style.display = 'none';
@@ -244,9 +244,9 @@ const handleKeydown = function(event: KeyboardEvent) {
 document.addEventListener('keydown', handleKeydown);
 
 // Event listener for making key presses in Stage 1 and Stage 2
-document.addEventListener('keydown', function(event) {
+document.addEventListener('keydown', function (event) {
     if (!keyInputAllowed) return; // Ignore keyboard input if not allowed
-    
+
     event.preventDefault(); // Prevent default scrolling behavior of arrow keys
 
     if (event.key === 'ArrowLeft' || event.key === 'ArrowRight') {
@@ -285,8 +285,8 @@ function startMainStudy() {
     document.getElementById('pointCounter')!.innerText = points.toString(); // needs to be here in order for first trial to show correct values
     document.getElementById('game-display')!.style.display = 'block';
     document.getElementById('stage-1-options')!.style.display = 'block';
-    document.getElementById('stage-1-practice-instructions')!.style.display = 'none'; 
-    document.getElementById('stage-1-main-instructions')!.style.display = 'block'; 
+    document.getElementById('stage-1-practice-instructions')!.style.display = 'none';
+    document.getElementById('stage-1-main-instructions')!.style.display = 'block';
     document.getElementById('stage-2-options')!.style.display = 'none';
     currentStage = "mainStage1"
 }
@@ -296,7 +296,7 @@ export function endTask() {
     document.getElementById('game-status')!.style.display = "block";
     document.getElementById('status')!.innerText = "Game complete! You earned a total of " + points.toString() + " points. Thank you for participating!";
     document.getElementById('game-display')!.style.display = "none";
-    
+
     // setTimeout(() => { 
     //     let redirect_url = "http://prolific.com/completion_code_here";
     //     document.location.assign(redirect_url);
@@ -304,11 +304,11 @@ export function endTask() {
 
 }
 
-function saveResultsToCSV(results: { stage: string; round: number; choice: string; outcome: string; reward: number, points: number; rewardImage: string  }[]): void {
+function saveResultsToCSV(results: { stage: string; round: number; choice: string; outcome: string; reward: number, points: number; rewardImage: string }[]): void {
     const subjectId: string = getParameterByName('subject_id', window.location.href) || 'UnknownSubject';
     const timestamp: string = new Date().toISOString().replace(/:/g, '-');
     const filename: string = `data/two_step_task_results_${subjectId}_${timestamp}.csv`;
-    
+
     const csvHeader = "Stage,Round,Choice,Outcome,Reward,TotalPoints";
     const csvRows = results.map(result => `${result.stage},${result.round},${result.choice},${result.outcome},${result.reward},${result.points}`).join("\n");
     const csvContent = `data:text/csv;charset=utf-8,${csvHeader}\n${csvRows}`;
